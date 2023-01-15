e384b4a114f1c342ab9960c97bf8ddb922166410
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-15 14:19:11 +0000

    added questions to javascript question file

---
+++ starter/questions.js
@@ -0,0 +1,79 @@
+let quizQuestions = [
+    //QUESTION 1
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            "<script/> ",
+            "<scripting/>",
+            "<javascript/>",
+            "<s/>"
+        ],
+        correctAnswer: "<script/>",
+    }
+    ,
+    //QUESTION 2
+    {
+        question: "The external JavaScript file must contain the <script> tag",
+        answers: [
+            "true ",
+            "false",
+            ],
+        correctAnswer: "false",
+    }
+    ,
+    //QUESTION 3
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: [
+            "alertBox('Hello World')",
+            "alert('Hello World')",
+            "popUp('Hello World')",
+            "alertPopUp('Hello World')",
+            ],
+        correctAnswer: "alert('Hello World')",
+    }
+    ,
+    //QUESTION 4
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: [
+            "function = myFunction()",
+            "function myFunction()",
+            "function:myFunction()",
+            ],
+        correctAnswer: "function = myFunction()",
+    }
+    ,
+    //QUESTION 5
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: [
+            "function = myFunction()",
+            "function myFunction()",
+            "function:myFunction()",
+            ],
+        correctAnswer: "function = myFunction()",
+    }
+    ,
+    //QUESTION 6
+    {
+        question: "How can you add a comment in a JavaScript?",
+        answers: [
+            "This is a comment",
+            "//This is a comment",
+            "<!--This is a comment-->",
+            ],
+        correctAnswer: "//This is a comment",
+    }
+    ,
+    //QUESTION 7
+    {
+        question: "Is JavaScript case-sensitive?",
+        answers: [
+            "true ",
+            "false",
+            ],
+        correctAnswer: "false",
+    }
+]
+    
