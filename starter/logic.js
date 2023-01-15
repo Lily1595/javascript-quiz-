6fa3e5893957d737229880d0d1b9ac51319ccc68
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-15 15:28:07 +0000

    declared global variables and set text content of question-title to question.js file

--- starter/logic.js
+++ starter/logic.js
@@ -1,3 +1,8 @@
+//DECLARE TIMER GLOBALLY 
+let timer = 60;
+//DECLARE SCORE GLOBALLY 
+let score = 0;
+
 //WHEN THE START BUTTON IS PRESSED, THE GAME BEGINS 
 document.getElementById("start").addEventListener("click", startGame)
 
@@ -6,3 +11,6 @@ function startGame() {
     document.getElementById("start-screen").classList.add("hide");
     document.getElementById("questions").classList.remove("hide");
 }
+
+//DISPLAY THE QUESTION 
+document.querySelector("#question-title").textContent = quizQuestions[0].question;
