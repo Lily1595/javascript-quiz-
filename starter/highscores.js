5d9ca5b246ca7a4ab1dfdac15ba8263a06fa7959
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-17 22:02:23 +0000

    added high scores code, have yet to link submit button to HS display

---
+++ starter/highscores.js
@@ -0,0 +1,18 @@
+// Get high scores from local storage
+let highScores = JSON.parse(localStorage.getItem("final")) || [];
+
+// Sort high scores from highest to lowest
+highScores.sort(function(a, b) {
+  return b.score - a.score;
+});
+
+// Get the container element where the high scores will be displayed
+let highScoreContainer = document.getElementById("highscores");
+
+// Loop through the high scores and create a list element for each one
+for (let i = 0; i < highScores.length; i++) {
+  let highScore = highScores[i];
+  let highScoreList = document.createElement("li");
+  highScoreList.innerHTML = highScore.initials + ": " + highScore.score;
+  highScoreContainer.appendChild(highScoreList);
+}
