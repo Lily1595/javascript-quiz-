bf7c198508bdbc486819a5c1d6c45b97dcef6b91
Lily Halladay <lilyhalladay14@gmail.com>
2023-01-17 11:26:37 +0000

    working on saving initials to local storage

--- starter/logic.js
+++ starter/logic.js
@@ -1,3 +1,9 @@
+/*
+TO DEBUG: 
+1. TIMER IS GOING INTO MINUS NUMBERS
+2. CHECK ANSWER FUNCTION ISN'T WORKING PROPERLY 
+3. NOT MOVING TO THE NEXT QUESTION ONCE BUTTON IS PRESSED 
+*/
 
 // INITIALISE JS VARIABLES 
 let startButton = document.querySelector("#start-screen");
@@ -8,6 +14,8 @@ let choicesEl = document.querySelector("#choices");
 let feedback = document.querySelector("#feedback");
 let endScreen = document.querySelector("#end-screen");
 let finalScore = document.querySelector("#final-score");
+let submit = document.querySelector("#submit");
+let initials = document.querySelector("#initials");
 
 //DECLARE TIMER GLOBALLY 
 let secondsLeft = 60;
@@ -54,26 +62,26 @@ function displayChoices() {
     for (let i = 0; i < choices.length; i++) {
         let choiceBtn = document.createElement("button");
         choiceBtn.textContent = choices[i];
-        choiceBtn.setAttribute('class', 'choice');
         choicesEl.appendChild(choiceBtn);
+        choicesEl.addEventListener("click", checkAnswer);
+        choicesEl.addEventListener("click", function(){
+            currentQuestion++;
+        });
 }
 } 
-choicesEl.onclick = checkAnswer;
 
-//CHECK ANSWER FUNCTION 
-function checkAnswer(event) {
-    let buttonEl = event.target;
-    if(!buttonEl.matches('.choice')){
-        return
-    }
+//CHECK ANSWER FUNCTION
+function checkAnswer(e) {
+    let selectedAnswer = e.target.innerHTML;
+    console.log(selectedAnswer);
     let correctAnswer = quizQuestions[currentQuestion].answer;
-    if (buttonEl.value === correctAnswer){
-        score++;
-        currentQuestion++;
-    }
+    if (selectedAnswer === correctAnswer){
+    console.log ("correct");
+    score++;
+}
     else {
+        console.log("this was not correct")
         secondsLeft -= 10;
-        currentQuestion++;
     }
 }
 
@@ -85,6 +93,112 @@ function endGame() {
     finalScore.innerHTML = score;
 }
 
-//SAVE INITIALS AND SCORE TO LOCAL STORAGE FUNCTION 
-function storeHighscores() {
+//SAVE INITIALS AND SCORE THIS IN LOCAL STORAGE FUNCTION 
+submit.addEventListener("click", saveHighScore); 
+let inputIntials = initials.value; 
+
+function saveHighScore() {
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function storeHighscores() 
+    let initialsInput = initials.value;
+submit.addEventListener("click", function() {})*/
+
+
+
+     /* Conditions to check the correct and wrong answers
+    if (userAnswer.matches("button")) {
+      if (index < questions.length - 1 && timeLeft > 0) {
+        if (key === correctAnswer) {
+          feedback.textContent = "Correct!";
+          correctSound.play();
+        } else {
+          feedback.textContent = "Wrong!";
+          wrongSound.play();
+          timeLeft -= 20;
+        }
+        index++;
+      } else {
+        if (key !== correctAnswer) {
+          timeLeft -= 20;
+        }
+
+*/
+/*
+let userAnswer = e.target;
+    let btnKey = userAnswer.innerText;
+    console.log(btnKey);
+    if (btnKey === quizQuestions[currentQuestion].answer){
+    console.log ("correct");
+    score++;
+}
+    else {
+        console.log("this was not correct")
+        secondsLeft -= 10;
+    }
+    currentQuestion++;
+}
+*/
+//CHECK ANSWER FUNCTION
+/*
+function checkAnswer(event) {
+    let buttonEl = event.target;
+    if(!buttonEl.matches(".choice-button")){
+        return
+    }
+    let correctAnswer = quizQuestions[currentQuestion].answer;
+    if (buttonEl.value === correctAnswer){
+        console.log("this was correct")
+        score++;    }
+    else {
+        console.log("this was not correct")
+        secondsLeft -= 10;
+    }
+    currentQuestion++;
+}
+
+//CHECK ANSWER FUNCTION 2 
+function checkAnswer(event) {
+    let correctAnswer = quizQuestions[currentQuestion].answer;
+    if (correctAnswer === event.target.textContent) {
+        console.log("this was correct")
+        score++;  
+    } else {
+        console.log("this was not correct")
+        secondsLeft -= 10;}
+     currentQuestion++; 
+    }
+    
+
+//CHECK ANSWER FUNCTION 3 
+function checkAnswer(event) {
+        let usersAnswer = event.target;
+        let correctAnswer = quizQuestions[currentQuestion].answer;
+        if (usersAnswer.value === correctAnswer){
+            console.log("this was correct")
+            score++;    }
+        else {
+            console.log("this was not correct")
+            secondsLeft -= 10;
+        }
+        currentQuestion++;
+    }
+
+    */
